0819df6322be540d0899ae5ebf068c386e6d16f8
Bao Trieu <tqb.mrga@gmail.com>
2020-02-20 00:07:38 +0700

    update new

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const mongoose = require('mongoose');
 const http = require('http');
 const port = process.env.PORT || 3000;
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/json
+app.use(bodyParser.json());
 
 //middlewares
 // app.use('/posts', (req,res) => {
 //     res.send('this is middlewares');
 // });
+
+// connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, { 
+        // user: 'root', //testroot
+        // pass: 123456, //password
+        // dbName: 'test', // 'mydb' which is the default selected DB
+        // autoIndex: 'NODE_ENV' === 'dev',
+        // useNewUrlParser: true,
+        // reconnectTries: Number.MAX_VALUE,
+        // reconnectInterval: 500, // Reconnect every 500ms
+        // poolSize: 10 // Maintain up to 10 socket connections
+     }, () => console.log('Connect to DB')
+);
 
 //Import Routers
 const postsRoute = require('./routes/posts');
@@ -20,12 +38,6 @@ app.get('/',(req,res) => {
     res.send('hello lulu');
 });
 
-
-// connect to DB
-mongoose.connect(
-    process.env.DB_CONNECTION, { useNewUrlParser: true }, () => 
-    console.log('Connect to DB')
-);
  
 // start listening
 app.listen(port);
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,34 @@
 const express = require('express');
-
 const router = express.Router();
+const Posts = require('../models/Posts');
 
 router.get('/',(req,res) => {
     res.send('hello posts');
 }); 
 
-router.get('/test',(req,res) => {
-    res.send('hello test');
+router.post('/',(req,res) => {
+    // res.setHeader('Content-Type', 'application/json')    
+    // res.end(JSON.stringify(req.body, null, 2))
+    
+    const post = new Posts({
+        title : req.body.title,
+        description : req.body.description
+    });
+    console.log(post);    
+    post.save()        
+        .then(data => {
+            res.json(data);
+            console.log('save');
+        })
+        .catch(err => {
+            res.json({message : err})
+            console.log('not save');
+        });
 }); 
+
+// router.get('/test',(req,res) => {
+//     res.send('hello test');
+// }); 
 
 
 module.exports = router;
